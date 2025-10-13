98be05377806ac48cee4c865928ccfbef3b346d6
codexpro.dev <chimeziev900@gmail.com>
2025-10-13 07:17:58 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ButtonGradient from './assets/svg/ButtonGradient.jsx'
+import Button from './components/Button.jsx'
+import Header from './components/Header.jsx'
+import "./index.css"
+
+const App = () => {
+  return (
+    <>
+    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+      <Header />
+    </div>
+    <ButtonGradient />
+    </>
+  )
+}
+
+export default App
