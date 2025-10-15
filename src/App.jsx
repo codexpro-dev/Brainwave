8a505281ed11f04ecf08e27ffd5d398816a82fe5
codexpro.dev <chimeziev900@gmail.com>
2025-10-15 03:28:51 -0800

    Brainwave

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import ButtonGradient from './assets/svg/ButtonGradient.jsx'
 import Benefits from './components/Benefits.jsx'
 import Button from './components/Button.jsx'
 import Collaboration from './components/Collaboration.jsx'
+import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
 import Hero from './components/Hero.jsx'
+import Pricing from './components/Pricing.jsx'
+import Roadmap from './components/Roadmap.jsx'
 import Services from './components/Services.jsx'
 import "./index.css"
 
@@ -16,6 +19,9 @@ const App = () => {
       <Benefits />
       <Collaboration />
       <Services />
+      <Pricing />
+      <Roadmap />
+      <Footer />
     </div>
     <ButtonGradient />
     </>
